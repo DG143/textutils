94a9691f75c35e81acb502d837925f7126de14bb
DG143 <97294518+DG143@users.noreply.github.com>
2024-06-24 18:07:11 +0530

    Update TextForm.js
    Button spacing done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -55,12 +55,12 @@ export default function TextForm(props) {
                 
                 <textarea className="form-control" value= {text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white' , color :props.mode === 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To Upper Case</button>
-            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert To Lower Case</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert To Upper Case</button>
+            <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert To Lower Case</button>
+            <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
             {/* <button className="btn btn-primary mx-2" onClick={handleHindiClick}>Convert to hindi script</button> */}
-            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
+            <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpace}>Remove Extra Space</button>
             
 
         </div>
